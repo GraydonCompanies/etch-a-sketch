9c6b51cb69fc6226c521fd9b3f57990af7e9277b
Graydon <jrapisardi@graydoncompanies.com>
2022-08-21 11:42:53 -0400

    Added destroyGrid function and documented code

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 function createGrid(rows, columns) {
-    const mainContainer = document.querySelector('.mainContainer');
 
-    for (let r = 0; r < rows; r++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.className = 'gridRow';
-        mainContainer.appendChild(rowDiv);
-        for (let c = 0; c < columns; c++) {
-            const columnDiv = document.createElement('div');
-            columnDiv.className = 'gridBox';
-            rowDiv.appendChild(columnDiv); 
-        }
+    //Check that input does not exceed 100 rows or columns
+    if (rows > 100 || columns > 100) {
+        return 'Please enter a parameter from 1 to 100';
     }
 
-    console.log(mainContainer);
+    //Create a reference to the mainContainer node
+    const mainContainer = document.querySelector('.mainContainer');
+
+    //Create the row/column structure
+    for (let r = 0; r < rows; r++) {
+        const gridRow = document.createElement('div');
+        gridRow.className = 'gridRow';
+        mainContainer.appendChild(gridRow);
+        for (let c = 0; c < columns; c++) {
+            const gridColumn = document.createElement('div');
+            gridColumn.className = 'gridColumn';
+            gridRow.appendChild(gridColumn);
+        }
+    }
+}
+
+function destroyGrid() {
+
+    //Create a reference to the mainContainer and all gridRow nodes
+    const mainContainer = document.querySelector('.mainContainer');
+    const gridRows = document.querySelectorAll('.gridRow');
+
+    //Destroy the rows
+    for (gridRow of gridRows) {
+        mainContainer.removeChild(gridRow);
+    }
 }
